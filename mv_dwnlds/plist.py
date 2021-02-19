cd68a6c5a08a365e2ca310dd04b7dbcf8985f878
Pablo Ordorica Wiener <pablo.ordoricaw@gmail.com>
2021-02-19 16:18:53 -0500

    Refactored imports & write plist file to user launch agents directory

--- mv_dwnlds/plist.py
+++ mv_dwnlds/plist.py
@@ -1,18 +1,13 @@
-import os
+from os.path import dirname, abspath, join
+from os import getcwd
 from plistlib import dump
 
 import utils
 
-if __name__ == "__main__":
-    d = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    config = utils.read_config(os.path.join(d, './config.yml'))
-
-    plist_name = config["plist_name"]
-    python_env = config["python_env"]
-
+def write_plist(plist_path, plist_name, python_env):
     plist = dict(
         Label="plist_name",
-        WorkingDirectory=os.getcwd() + "/",
+        WorkingDirectory= dirname(abspath(__file__)) + "/",
         ProgramArguments= [
             python_env,
             "mv_dwnlds.py"
@@ -20,6 +15,18 @@ if __name__ == "__main__":
         RunAtLoad=True,
         KeepAlive=True
     )
+    plist_file_name = plist_name + ".plist"
+    plist_file_path = join(plist_path, plist_file_name)
 
-    with open(plist_name + ".plist", "wb") as fp:
+    with open(plist_file_path, "wb") as fp:
         dump(plist, fp)
+
+if __name__ == "__main__":
+    d = dirname(dirname(abspath(__file__)))
+    config = utils.read_config(join(d, './config.yml'))
+
+    plist_name = config["plist_name"]
+    python_env = config["python_env"]
+    launch_agent_dir = config["launch_agent_dir"]
+
+    write_plist(launch_agent_dir, plist_name, python_env)
